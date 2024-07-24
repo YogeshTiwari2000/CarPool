6143dfe85e950cb4078235cc1aecf1313464d684
Dewanshu <dewanshu.singh@paavu.com>
2024-07-24 19:05:32 +0530

    login modal & forgot modal added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { IonApp, IonSplitPane, IonMenu, IonContent, IonList, IonListHeader, IonNote, IonMenuToggle, IonItem, IonIcon, IonLabel, IonRouterOutlet, IonRouterLink } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp } from 'ionicons/icons';
+import { mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, close } from 'ionicons/icons';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +21,6 @@ export class AppComponent {
   ];
 
   constructor() {
-    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp });
+    addIcons({ mailOutline, mailSharp, paperPlaneOutline, paperPlaneSharp, heartOutline, heartSharp, archiveOutline, archiveSharp, trashOutline, trashSharp, warningOutline, warningSharp, bookmarkOutline, bookmarkSharp, close });
   }
 }
--- src/app/modals/login/login.component.ts
+++ src/app/modals/login/login.component.ts
@@ -1,19 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { IonInput, IonInputPasswordToggle, IonItem } from "@ionic/angular/standalone";
+import { Component, inject, OnInit } from '@angular/core';
+import { IonInput, IonInputPasswordToggle, IonContent, IonHeader, IonToolbar, IonTitle, ModalController, IonIcon, IonButton } from "@ionic/angular/standalone";
+import { ForgotPassComponent } from '../forgot-pass/forgot-pass.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [IonItem, IonInput, IonInputPasswordToggle]
+  imports: [IonButton, IonIcon, IonTitle, IonToolbar, IonHeader, IonContent, IonInput, IonInputPasswordToggle]
 })
 export class LoginComponent implements OnInit {
+  public modalCtrl = inject(ModalController)
 
   constructor() { }
 
   ngOnInit() {
     console.log("Login Modal");
+  }
+  close() {
+    return this.modalCtrl.dismiss(null, 'close');
+  }
+  async forgotPassModal() {
+    const modal = await this.modalCtrl.create({
+      component: ForgotPassComponent,
+      cssClass: ["forgotPassModalCss", "ion-padding-horizontal"],
+      showBackdrop: true
+    })
+    modal.present()
   }
 
 }
