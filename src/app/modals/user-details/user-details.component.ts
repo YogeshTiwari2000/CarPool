71b58f54d5410fc1a7c9f01a25861d8cc4a48d81
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-07-25 16:03:43 +0530

    card-details in progress

--- src/app/modals/user-details/user-details.component.ts
+++ src/app/modals/user-details/user-details.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, IonButton, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/angular/standalone';
+import { ModalController, IonButton, IonGrid, IonRow, IonCol, IonIcon, IonCard, IonCardHeader, IonCardContent, IonItem, IonAvatar, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
   standalone: true,
-  imports: [IonIcon, IonCol, IonRow, IonGrid, IonButton,],
+  imports: [IonLabel, IonAvatar, IonItem, IonCardContent, IonCardHeader, IonCard, IonIcon, IonCol, IonRow, IonGrid, IonButton,],
 })
 export class UserDetailsComponent implements OnInit {
 
