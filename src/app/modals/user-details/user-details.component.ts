3bd4d60aa2641ab11416cc2823031802e13f482f
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-07-25 15:34:49 +0530

    image added

--- src/app/modals/user-details/user-details.component.ts
+++ src/app/modals/user-details/user-details.component.ts
@@ -1,30 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
   styleUrls: ['./user-details.component.scss'],
   standalone: true,
-  imports: [IonButton,],
+  imports: [],
 })
 export class UserDetailsComponent implements OnInit {
 
-  constructor(public modalCtrl: ModalController) { }
+  constructor() { }
 
   ngOnInit() {
-    console.log("user details");
+    console.log();
+
   }
-
-  close() {
-    this.modalCtrl.dismiss()
-  }
-
-
-
-
-
-
-
 
 }
