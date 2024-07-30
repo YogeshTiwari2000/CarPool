2dd4279b22aaa52731359cf720ef8bbb20333535
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-07-30 17:46:46 +0530

    about

--- src/app/pages/profile/about/about.page.ts
+++ src/app/pages/profile/about/about.page.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardHeader, IonAvatar, IonCardTitle, IonCardSubtitle, IonCardContent, IonItem, IonIcon, IonLabel } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.page.html',
   styleUrls: ['./about.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonLabel, IonIcon, IonItem, IonCardContent, IonCardSubtitle, IonCardTitle, IonAvatar, IonCardHeader, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AboutPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    console.log("about")
   }
 
 }
