01b24e3761ca15d4d3f54eb6e68d6f01765ecb45
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-01 17:49:26 +0530

    profile

--- src/app/pages/profile/account/account.page.ts
+++ src/app/pages/profile/account/account.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonCardHeader, IonCard } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonLabel, IonCardHeader, IonCard, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
-  imports: [IonCard, IonCardHeader, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonCard, IonCardHeader, IonLabel, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AccountPage implements OnInit {
 
