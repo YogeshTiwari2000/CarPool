ddbae3be6f6b4e8f96b594bf9f8752cc1cef0aa2
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-01 18:51:44 +0530

    feeedback

--- src/app/pages/feedback/feedback.page.ts
+++ src/app/pages/feedback/feedback.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabButton, IonLabel, IonIcon, IonButton, IonRow, IonCol, IonItem, IonTextarea, IonSelect, IonSelectOption, IonList } from '@ionic/angular/standalone';
+import { HandleDataService } from 'src/app/services/data/handle-data.service';
 
 @Component({
   selector: 'app-feedback',
@@ -11,19 +12,45 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabButton, IonLabel, Io
   imports: [IonList, IonTextarea, IonItem, IonCol, IonRow, IonButton, IonIcon, IonLabel, IonTabButton, IonContent, IonHeader, IonTitle, IonToolbar, IonSelect, IonSelectOption, CommonModule, FormsModule]
 })
 export class FeedbackPage implements OnInit {
-  rating: any;
-  email: any = 'sholetshubham8@gmail.com'
+  dataservice = inject(HandleDataService)
+  star: any;
+  email: any = 'dewanshusingh21@gmail.com'
   constructor() { }
   stars: any = ['1', '2', '3', '4', '5'];
   filledStars: boolean[] = [false, false, false, false, false];
 
+  feedbackFields = {
+    name: '',
+    email: '',
+    photo: '',
+    star: '',
+    feedback: '',
+    comment: '',
+  }
+
+  rating = {
+    given: [{
+      ...this.feedbackFields
+    }],
+    recieved: [{
+      ...this.feedbackFields
+    }]
+  }
+
+
+
   ngOnInit() {
+    console.log('this.dataservice.user',
+
+      this.dataservice.checkUserExists(this.email)
+
+    );
 
   }
 
   onStarClick(index: number) {
     console.log('Star index:', index + 1);
-    this.rating = index + 1;
+    this.star = index + 1;
     for (let i = 0; i < this.stars.length; i++) {
       this.filledStars[index] = true;
       this.filledStars[i] = i <= index;
