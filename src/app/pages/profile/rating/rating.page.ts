68512b6d377293ae9a6ceb6bcd8eded6866f52d7
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-02 17:48:45 +0530

    rating

--- src/app/pages/profile/rating/rating.page.ts
+++ src/app/pages/profile/rating/rating.page.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCard } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCard, IonIcon } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.page.html',
   styleUrls: ['./rating.page.scss'],
   standalone: true,
-  imports: [IonCard, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonCard, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class RatingPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    console.log("rating");
+
   }
 
 }
