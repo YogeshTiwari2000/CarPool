fdf914b76c8808ddc7ee045b8879d284ed72467b
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-02 11:29:26 +0530

    profile

--- src/app/pages/profile/profile.routes.ts
+++ src/app/pages/profile/profile.routes.ts
@@ -1,17 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'about',
-        loadComponent: () => import('./about/about.page').then(m => m.AboutPage)
-    },
-    {
-        path: 'account',
-        loadComponent: () => import('./account/account.page').then(m => m.AccountPage)
-    },
-    {
-        path: '',
-        redirectTo: 'about',
-        pathMatch: 'full',
-    }
+  {
+    path: 'about',
+    loadComponent: () => import('./about/about.page').then(m => m.AboutPage)
+  },
+  {
+    path: 'account',
+    loadComponent: () => import('./account/account.page').then(m => m.AccountPage)
+  },
+  {
+    path: '',
+    redirectTo: 'about',
+    pathMatch: 'full',
+  },
+
 ]
