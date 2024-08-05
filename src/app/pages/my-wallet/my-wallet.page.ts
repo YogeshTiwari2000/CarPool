acb8d436aa08d339804b9bd4112af7aff684f7d7
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-05 18:28:53 +0530

    wallet

--- src/app/pages/my-wallet/my-wallet.page.ts
+++ src/app/pages/my-wallet/my-wallet.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardHeader, IonAvatar, IonCardSubtitle, IonCardTitle } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCardHeader, IonAvatar, IonCardSubtitle, IonCardTitle, IonCard, IonItem, IonText, IonLabel, IonIcon, IonCol, IonRow, IonGrid, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-my-wallet',
   templateUrl: './my-wallet.page.html',
   styleUrls: ['./my-wallet.page.scss'],
   standalone: true,
-  imports: [IonCardTitle, IonCardSubtitle, IonAvatar, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonList, IonGrid, IonRow, IonCol, IonIcon, IonLabel, IonText, IonItem, IonCard, IonCardTitle, IonCardSubtitle, IonAvatar, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class MyWalletPage implements OnInit {
 
