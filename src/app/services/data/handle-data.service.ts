65111f4a7942205f1a302681c14d577524b48a79
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-05 12:07:07 +0530

    existing user find

--- src/app/services/data/handle-data.service.ts
+++ src/app/services/data/handle-data.service.ts
@@ -6,6 +6,9 @@ import { map, Observable } from "rxjs";
   providedIn: "root",
 })
 export class HandleDataService {
+  getUser(): any {
+    throw new Error('Method not implemented.');
+  }
   public http = inject(HttpClient);
   private apiUrl = "http://localhost:3000/users";
 
@@ -22,12 +25,12 @@ export class HandleDataService {
     return this.http.get<any[]>(this.apiUrl);
   }
 
-  removeKeys(obj:any, key:any) {
+  removeKeys(obj: any, key: any) {
     if (obj.hasOwnProperty(key)) {
       delete obj[key];
+    }
+    return obj;
   }
-  return obj;
-}
 
   checkUserExists(email: string): Observable<boolean> {
     return this.http.get<any[]>(this.apiUrl).pipe(
@@ -36,14 +39,18 @@ export class HandleDataService {
         if (users) {
           return users.some(user => {
             console.log("user === ", user);
-            
-            this.user = this.removeKeys(user,"id");
+
+            this.user = this.removeKeys(user, "id");
             console.log(" this.user === ", this.user);
             const userKeys = Object.keys(this.user);
             // console.log("userKeys === ", userKeys);
             return userKeys.some(key => {
-              const userData = this.user[key];
-              return userData.userEmail === email;
+              const userData = user[key];
+              if (userData.userEmail === email) {
+                const currentUser = userData
+                this.getExistingUserData(currentUser)
+                return currentUser;
+              }
             });
           });
         } else {
@@ -52,6 +59,15 @@ export class HandleDataService {
       })
     );
   }
+
+  getExistingUserData(userData: any) {
+    console.log("dsatd", this.user = userData);
+    return this.user = userData
+  }
+
+
+
+
 
   addUser(user: any): Observable<any> {
     return this.http.post(this.apiUrl, user);
