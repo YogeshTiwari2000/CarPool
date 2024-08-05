d5c6d10af4da47370f26b306249b91fc475ef54a
Dewanshu <dewanshu.singh@paavu.com>
2024-08-05 13:48:41 +0530

    profile page upated

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,23 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabs, IonTabBar, IonTabButton, IonIcon, IonButtons, IonMenuButton, IonBackButton, IonBreadcrumb, IonItem } from '@ionic/angular/standalone';
-import { Router } from '@angular/router';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonTabs, IonTabBar, IonTabButton, IonIcon, IonButtons, IonMenuButton, IonBackButton, IonItem, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonLabel, ModalController, IonAvatar, IonCardSubtitle } from '@ionic/angular/standalone';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { EditCardComponent } from 'src/app/modals/edit-card/edit-card.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.scss'],
   standalone: true,
-  imports: [IonItem, IonBreadcrumb, IonBackButton, IonButtons, IonIcon, IonMenuButton, IonTabButton, IonTabBar, IonTabs, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardSubtitle, IonAvatar, IonLabel, IonCardContent, IonCardTitle, IonCardHeader, IonCard, IonItem, IonBackButton, IonButtons, IonIcon, IonMenuButton, IonTabButton, IonTabBar, IonTabs, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink, RouterLinkActive]
 })
 export class ProfilePage implements OnInit {
 
-  constructor(private routes: Router) { }
+  // constructor(private routes: Router) { }
+  public router = inject(Router)
+  public modalCtrl = inject(ModalController)
+
+  currentUser: any = ''
+  isEmailVerified: boolean = false
+  isPhoneVerified: boolean = false
+  isGovtIdVerified: boolean = false
+
+  constructor() { }
 
   ngOnInit() {
-    console.log("profile");
-    this.routes.navigate(['profile/about']);
+    const data: any = localStorage.getItem('currentUser');
+    const parsedData: any = JSON.parse(data)
+    console.log("data === ", parsedData);
+
+    const keys = Object.keys(parsedData);
+    console.log("keys === ", keys);
+    // const firstKey = keys[0];
+    // const firstKey = keys[1];
+    this.currentUser = parsedData;
+    console.log('about page', this.currentUser);
+
+    this.isEmailVerified = this.currentUser.email_verified ? true : false
+    this.isPhoneVerified = this.currentUser.phone_verified ? true : false
+    this.isGovtIdVerified = this.currentUser.govtId_verified ? true : false
   }
+
+  async openEditCard() {
+    const modal = await this.modalCtrl.create({
+      component: EditCardComponent,
+      componentProps: { data: this.currentUser }
+    })
+
+    modal.onDidDismiss().then((dataFromModal) => {
+      if (dataFromModal !== null) {
+        console.log('The result:', dataFromModal.data);
+
+      }
+    })
+
+    return await modal.present();
+
+
+  }
+
 
 }
--- src/app/pages/profile/profile.routes.ts
+++ src/app/pages/profile/profile.routes.ts
@@ -7,20 +7,12 @@ export const routes: Routes = [
     component: ProfilePage,
     children: [
       {
-        path: 'about',
-        loadComponent: () => import('./about/about.page').then(m => m.AboutPage)
-      },
-      {
-        path: 'account',
-        loadComponent: () => import('./account/account.page').then(m => m.AccountPage)
-      },
-      {
         path: 'rating',
         loadComponent: () => import('./rating/rating.page').then(m => m.RatingPage)
       },
       {
         path: '',
-        redirectTo: 'profile/about',
+        redirectTo: 'profile',
         pathMatch: 'full',
       },
     ]
