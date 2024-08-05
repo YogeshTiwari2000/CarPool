862c051ea145bcb6df640bcaa9ef99faf6ce5a40
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-05 13:39:12 +0530

    rating page

--- src/app/pages/profile/rating/rating.page.ts
+++ src/app/pages/profile/rating/rating.page.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCard, IonIcon, IonCardHeader, IonCardSubtitle, IonAvatar, IonCardTitle, IonButtons, IonBackButton, IonBreadcrumb, IonBreadcrumbs } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonCard, IonIcon, IonCardHeader, IonCardSubtitle, IonAvatar, IonCardTitle, IonButtons, IonBackButton } from '@ionic/angular/standalone';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-rating',
   templateUrl: './rating.page.html',
   styleUrls: ['./rating.page.scss'],
   standalone: true,
-  imports: [IonBreadcrumb, IonButtons, IonCardTitle, IonAvatar, IonCardSubtitle, IonCardHeader, IonIcon, IonCard, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, IonBreadcrumbs]
+  imports: [IonButtons, IonCardTitle, IonAvatar, IonCardSubtitle, IonCardHeader, IonIcon, IonCard, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonBackButton, RouterLink, RouterLinkActive]
 })
 export class RatingPage implements OnInit {
 
