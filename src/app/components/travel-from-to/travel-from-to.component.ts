9eba94cce33680073a5b9a81b10224a408643be7
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-06 16:42:49 +0530

    travel component

---
+++ src/app/components/travel-from-to/travel-from-to.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travel-from-to',
+  templateUrl: './travel-from-to.component.html',
+  styleUrls: ['./travel-from-to.component.scss'],
+  standalone: true,
+  imports: [],
+})
+export class TravelFromToComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
