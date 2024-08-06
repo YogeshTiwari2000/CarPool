05e7c8b69f0262c3d9a9cdba7421156e92936dc5
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-06 16:43:24 +0530

    wallet

--- src/app/pages/my-wallet/my-wallet.page.ts
+++ src/app/pages/my-wallet/my-wallet.page.ts
@@ -10,184 +10,36 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './my-wallet.page.html',
   styleUrls: ['./my-wallet.page.scss'],
   standalone: true,
-  imports: [IonImg, IonList, IonGrid, IonRow, IonCol, IonIcon, IonLabel, IonText, IonItem, IonCard, IonCardTitle, IonCardSubtitle, IonAvatar, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,RouterLink]
+  imports: [IonImg, IonList, IonGrid, IonRow, IonCol, IonIcon, IonLabel, IonText, IonItem, IonCard, IonCardTitle, IonCardSubtitle, IonAvatar, IonCardHeader, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, RouterLink]
 })
 export class MyWalletPage implements OnInit {
 
-  constructor(private router :Router) {
+  userData: any;
+  transactions: any[] = [];
+  balance: number = 0;
+
+  constructor(private router: Router) {
     console.log(data);
-
+    console.log("mydata === ", this.mydata);
   }
+  mydata = data;
 
-  dummy_data = [
-    {
-      "id": "f2a7",
-      "dewanshu.singh@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocIkgegYs_b9uOr78xy-AaQE9EPi3hYFubLchzJ2_F7Mw-LD70M=s96-c",
-        "userEmail": "dewanshu.singh@paavu.com",
-        "userName": "Dewanshu Singh",
-        "password": "",
-        "cpassword": "",
-        "phone": "",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true,
-        "wallet": {
 
-        }
-      }
-    },
-    {
-      "id": "8e52",
-      "shubham.sholet@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocL13r6ZNdJKOi6XRnVuHCVIR6ozMda-5NEr8u2TgeiP-NpuLg=s96-c",
-        "userEmail": "shubham.sholet@paavu.com",
-        "userName": "Shubham Sholet",
-        "password": "",
-        "cpassword": "",
-        "phone": "",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true
-      }
-    },
-    {
-      "id": "35f1",
-      "yogesh.tiwari@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocK-soA9fgCZmHQGvuHO_ZjoVDIOYNQ9Cnj-2ID34eo0TvwQCAI=s96-c",
-        "userEmail": "yogesh.tiwari@paavu.com",
-        "userName": "Yogesh Tiwari",
-        "password": "",
-        "cpassword": "",
-        "phone": "",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true
-      }
-    },
-    {
-      "id": "abdf",
-      "yogi.baba@baba.com": {
-        "profilePicture": "",
-        "userEmail": "yogi.baba@baba.com",
-        "userName": "yogi",
-        "password": "baba.com",
-        "cpassword": "12420Y",
-        "phone": "9582467932",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": false,
-        "email_verified": false
-      }
-    },
-    {
-      "id": "b8fb",
-      "nikita.shukla@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocKnsPBYCSXMz8f56NCKg1uRUhVdm8sqBxjF9cfWimKfENOtrw=s96-c",
-        "userEmail": "nikita.shukla@paavu.com",
-        "userName": "Nikita Shukla",
-        "password": "",
-        "cpassword": "",
-        "phone": "",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true
-      }
-    },
-    {
-      "id": "5916",
-      "dewanshu.singh@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocIkgegYs_b9uOr78xy-AaQE9EPi3hYFubLchzJ2_F7Mw-LD70M=s96-c",
-        "userEmail": "dewanshu.singh@paavu.com",
-        "userName": "Dewanshu Singh",
-        "password": "",
-        "cpassword": "",
-        "phone": "1234536757",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true
-      }
-    },
-    {
-      "id": "7037",
-      "dewanshu.singh@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocIkgegYs_b9uOr78xy-AaQE9EPi3hYFubLchzJ2_F7Mw-LD70M=s96-c",
-        "userEmail": "dewanshu.singh@paavu.com",
-        "userName": "Dewanshu Singh",
-        "password": "",
-        "cpassword": "",
-        "phone": "2341234123",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true
-      }
-    },
-    {
-      "id": "c239",
-      "dewanshu.singh@paavu.com": {
-        "profilePicture": "https://lh3.googleusercontent.com/a/ACg8ocIkgegYs_b9uOr78xy-AaQE9EPi3hYFubLchzJ2_F7Mw-LD70M=s96-c",
-        "userEmail": "dewanshu.singh@paavu.com",
-        "userName": "Dewanshu Singh",
-        "password": "",
-        "cpassword": "",
-        "phone": "",
-        "pickUpLocation": "",
-        "dropLocation": "",
-        "isSocialLogin": true,
-        "email_verified": true
-      }
-    }
-  ]
   ngOnInit() {
-    console.log("wallet");
+    const currentUserEmail = 'yogesh.tiwari@paavu.com'; // Replace with actual logic
 
-    // this.lastSixTransaction = [
-    //   {
-    //     "Name": "Walmart",
-    //     "Date": "2024-08-06 12:32",
-    //     "Amount": "500",
-    //     "Type": "Deduction"
-    //   },
-    //   {
-    //     "Name": "Wallet Top up",
-    //     "Date": "2024-08-06 12:32",
-    //     "Amount": "1000",
-    //     "Type": "Addition"
-    //   },
-    //   {
-    //     "Name": "Refund From Netflix",
-    //     "Date": "2024-08-06 12:32",
-    //     "Amount": "20.20",
-    //     "Type": "Addition"
-    //   },
-    //   {
-    //     "Name": "Netflix Recharge",
-    //     "Date": "2024-08-06 12:32",
-    //     "Amount": "40",
-    //     "Type": "Deduction"
-    //   },
-    //   {
-    //     "Name": "Amazon",
-    //     "Date": "2024-08-06 12:32",
-    //     "Amount": "35.23",
-    //     "Type": "Deduction"
-    //   },
-    //   {
-    //     "Name": "Wallet Top Up",
-    //     "Date": "2024-08-06 12:32",
-    //     "Amount": "1000",
-    //     "Type": "Addition"
-    //   }
-    // ]
+    // Find the user data for the current user
+    const userEntry = data.find((user: any) => user[currentUserEmail]);
+    console.log(userEntry);
 
+    if (userEntry) {
+      this.userData = userEntry[currentUserEmail];
+      const userWallet = this.userData.wallet;
+      this.transactions = userWallet.transactions;
+      this.balance = userWallet.balance;
+    }
   }
-  goToWalletHistory(){
+  goToWalletHistory() {
     this.router.navigate(['/wallet-history'])
   }
 }
