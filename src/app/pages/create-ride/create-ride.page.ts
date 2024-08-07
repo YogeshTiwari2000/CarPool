f5fdd97ad70af2d4080a821e0776a2aaca07b5d8
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-07 18:47:45 +0530

    create ride

--- src/app/pages/create-ride/create-ride.page.ts
+++ src/app/pages/create-ride/create-ride.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonDatetimeButton, IonModal, IonDatetime, IonTabButton, IonButton, IonRadioGroup, IonRadio, IonItem, IonLabel } from '@ionic/angular/standalone';
 import { TravelFromToComponent } from 'src/app/components/travel-from-to/travel-from-to.component';
 
@@ -9,19 +9,38 @@ import { TravelFromToComponent } from 'src/app/components/travel-from-to/travel-
   templateUrl: './create-ride.page.html',
   styleUrls: ['./create-ride.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonItem, IonRadio, IonRadioGroup, IonButton, IonTabButton, IonDatetime, IonModal, IonDatetimeButton, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TravelFromToComponent]
+  imports: [IonLabel, IonItem, IonRadio, IonRadioGroup, IonButton, IonTabButton, IonDatetime, IonModal, IonDatetimeButton, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TravelFromToComponent, ReactiveFormsModule,]
 })
 export class CreateRidePage implements OnInit {
   time: string = '12:00';
-  date: any = ''
+
+  date: any = '';
   seatAvl: number = 1;
   price: number = 120;
-  constructor() { }
+  createRideForm: FormGroup;
+  constructor(private FormBuilder: FormBuilder) {
+    this.createRideForm = this.FormBuilder.group({
+      time: [Validators.required],
+      date: [Validators.required],
+      seatAvl: [Validators.required],
+      price: [Validators.required],
 
-  ngOnInit() {
+    });
   }
 
+  ngOnInit() {
+    this.setDateToCurrent()
+  }
 
+  setDateToCurrent() {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const yyyy = today.getFullYear();
+
+    // this.date = `${dd}-${mm}-${yyyy}`;
+    this.date = `${yyyy}-${mm}-${dd}`;
+  }
   incseatAvl() {
     if (this.seatAvl < 7) {
       this.seatAvl++
@@ -32,5 +51,12 @@ export class CreateRidePage implements OnInit {
     if (this.seatAvl > 1) {
       this.seatAvl--
     }
+  }
+  onCreateRide() {
+    console.log('onCreateRide  clicked');
+    console.log("time === ", this.time);
+    console.log("date === ", this.date);
+    console.log("seatAvl === ", this.seatAvl);
+    console.log("price === ", this.price);
   }
 }
