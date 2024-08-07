659220e7633db542c2734239af480e95e6ba5ebb
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-07 11:43:49 +0530

    travel component

--- src/app/components/travel-from-to/travel-from-to.component.ts
+++ src/app/components/travel-from-to/travel-from-to.component.ts
@@ -1,16 +1,90 @@
+declare var google: any;
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FormsModule } from '@angular/forms';
+import { IonCol, IonInput } from "@ionic/angular/standalone";
+import { GooglePlaceModule } from "ngx-google-places-autocomplete";
 
 @Component({
   selector: 'app-travel-from-to',
   templateUrl: './travel-from-to.component.html',
   styleUrls: ['./travel-from-to.component.scss'],
   standalone: true,
-  imports: [],
+  imports: [IonInput, IonCol, FormsModule, CommonModule],
+  providers: [GooglePlaceModule]
 })
 export class TravelFromToComponent implements OnInit {
+  to: any
+  from: any
+  place: any
+  constructor() {
+    this.fromLocation()
+    this.toLocation()
+  }
 
-  constructor() { }
+  ngOnInit() {
+    console.log("ngOnInit === ");
+  }
+  ionViewWillEnter() {
+    console.log("ionViewWillEnter === ");
+    this.fromLocation()
+    this.toLocation()
+  }
+  swapLocations() {
+    const temp = this.from;
+    this.from = this.to;
+    this.to = temp;
+  }
 
-  ngOnInit() { }
-
+  fromLocation() {
+    let inputElement = document.getElementById('fromLocation');
+    if (inputElement) {
+      let input = inputElement.getElementsByTagName('input')[0];
+      let autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'], componentRestrictions: { country: 'in' } });
+      google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        // retrieve the place object for your use
+        let place = autocomplete.getPlace();
+        console.log("google place called", place);
+        this.from = place.formatted_address;
+        // for (var i = 0; i < place.address_components.length; i++) {
+        //   for (var j = 0; j < place.address_components[i].types.length; j++) {
+        //     if (place.address_components[i].types[j] == "postal_code") {
+        //       this.pincode = place.address_components[i].long_name;
+        //     }
+        //     if (place.address_components[i].types[j] == "locality") {
+        //       this.city = place.address_components[i].long_name;
+        //       console.log("enter in locality if", this.city);
+        //     }
+        //   }
+        // }
+        // this.locationName = place.formatted_address;
+      });
+    }
+  }
+  toLocation() {
+    let inputElement = document.getElementById('toLocation');
+    if (inputElement) {
+      let input = inputElement.getElementsByTagName('input')[0];
+      let autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'], componentRestrictions: { country: 'in' } });
+      google.maps.event.addListener(autocomplete, 'place_changed', () => {
+        // retrieve the place object for your use
+        let place = autocomplete.getPlace();
+        console.log("google place called", place);
+        this.to = place.formatted_address;
+        // for (var i = 0; i < place.address_components.length; i++) {
+        //   for (var j = 0; j < place.address_components[i].types.length; j++) {
+        //     if (place.address_components[i].types[j] == "postal_code") {
+        //       this.pincode = place.address_components[i].long_name;
+        //     }
+        //     if (place.address_components[i].types[j] == "locality") {
+        //       this.city = place.address_components[i].long_name;
+        //       console.log("enter in locality if", this.city);
+        //     }
+        //   }
+        // }
+        // this.locationName = place.formatted_address;
+      });
+    }
+  }
 }
--- src/app/pages/search-screen/search-screen.page.ts
+++ src/app/pages/search-screen/search-screen.page.ts
@@ -6,16 +6,16 @@ import { OverlayEventDetail } from '@ionic/core/components';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow, IonCol, IonInput, IonButton, IonItem, IonLabel, IonModal, IonDatetime, IonDatetimeButton, IonIcon, IonButtons, IonMenuButton, IonImg } from '@ionic/angular/standalone';
 
 import { Router } from '@angular/router';
-import { GooglePlaceModule } from "ngx-google-places-autocomplete";
-declare var google: any;
+import { TravelFromToComponent } from 'src/app/components/travel-from-to/travel-from-to.component';
+
 
 @Component({
   selector: 'app-search-screen',
   templateUrl: './search-screen.page.html',
   styleUrls: ['./search-screen.page.scss'],
   standalone: true,
-  imports: [IonImg, IonButtons, IonIcon, IonDatetimeButton, IonDatetime, IonModal, IonLabel, IonItem, IonButton, IonInput, IonCol, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton],
-  providers: [GooglePlaceModule]
+  imports: [IonImg, IonButtons, IonIcon, IonDatetimeButton, IonDatetime, IonModal, IonLabel, IonItem, IonButton, IonInput, IonCol, IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton, TravelFromToComponent],
+
 })
 export class SearchScreenPage implements OnInit {
   @ViewChild(IonModal)
@@ -32,26 +32,16 @@ export class SearchScreenPage implements OnInit {
 
 
 
-  swapLocations() {
-    const temp = this.from;
-    this.from = this.to;
-    this.to = temp;
-  }
 
   isInputRequired: boolean = true;
-  to: any
-  from: any
-  place: any
 
-  ionViewWillEnter() {
-    this.fromLocation()
-    this.toLocation()
-  }
+
+
 
   search() {
     const searchData = {
-      from: this.from,
-      to: this.to,
+      // from: this.from,
+      // to: this.to,
       date: this.date,
       time: this.time,
       passengers: this.passengers
@@ -62,56 +52,6 @@ export class SearchScreenPage implements OnInit {
 
 
 
-  fromLocation() {
-    let inputElement = document.getElementById('fromLocation');
-    if (inputElement) {
-      let input = inputElement.getElementsByTagName('input')[0];
-      let autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'], componentRestrictions: { country: 'in' } });
-      google.maps.event.addListener(autocomplete, 'place_changed', () => {
-        // retrieve the place object for your use
-        let place = autocomplete.getPlace();
-        console.log("google place called", place);
-        this.from = place.formatted_address;
-        // for (var i = 0; i < place.address_components.length; i++) {
-        //   for (var j = 0; j < place.address_components[i].types.length; j++) {
-        //     if (place.address_components[i].types[j] == "postal_code") {
-        //       this.pincode = place.address_components[i].long_name;
-        //     }
-        //     if (place.address_components[i].types[j] == "locality") {
-        //       this.city = place.address_components[i].long_name;
-        //       console.log("enter in locality if", this.city);
-        //     }
-        //   }
-        // }
-        // this.locationName = place.formatted_address;
-      });
-    }
-  }
-  toLocation() {
-    let inputElement = document.getElementById('toLocation');
-    if (inputElement) {
-      let input = inputElement.getElementsByTagName('input')[0];
-      let autocomplete = new google.maps.places.Autocomplete(input, { types: ['geocode'], componentRestrictions: { country: 'in' } });
-      google.maps.event.addListener(autocomplete, 'place_changed', () => {
-        // retrieve the place object for your use
-        let place = autocomplete.getPlace();
-        console.log("google place called", place);
-        this.to = place.formatted_address;
-        // for (var i = 0; i < place.address_components.length; i++) {
-        //   for (var j = 0; j < place.address_components[i].types.length; j++) {
-        //     if (place.address_components[i].types[j] == "postal_code") {
-        //       this.pincode = place.address_components[i].long_name;
-        //     }
-        //     if (place.address_components[i].types[j] == "locality") {
-        //       this.city = place.address_components[i].long_name;
-        //       console.log("enter in locality if", this.city);
-        //     }
-        //   }
-        // }
-        // this.locationName = place.formatted_address;
-      });
-    }
-  }
   increment() {
     const counterElement = document.getElementById('open-modal-addPassanger');
     if (counterElement !== null) {
