a97c248a3cdd883f08031830f746265c8062ded2
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-07 12:35:52 +0530

    travel

--- src/app/pages/create-ride/create-ride.page.ts
+++ src/app/pages/create-ride/create-ride.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonRow } from '@ionic/angular/standalone';
+import { TravelFromToComponent } from 'src/app/components/travel-from-to/travel-from-to.component';
 
 @Component({
   selector: 'app-create-ride',
   templateUrl: './create-ride.page.html',
   styleUrls: ['./create-ride.page.scss'],
   standalone: true,
-  imports: [IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonRow, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TravelFromToComponent]
 })
 export class CreateRidePage implements OnInit {
 
