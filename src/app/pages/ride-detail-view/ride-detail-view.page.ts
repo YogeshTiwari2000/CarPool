4f26ebd8104d71dd2c79aa8b28ddf397a65d4cd5
Shubham Sholet <shubham.sholet@paavu.com>
2024-08-26 12:52:08 +0530

    ride-details-view

--- src/app/pages/ride-detail-view/ride-detail-view.page.ts
+++ src/app/pages/ride-detail-view/ride-detail-view.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCol, IonRow, IonButton, IonLabel, IonItem, IonIcon, IonButtons, IonMenuButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCol, IonRow, IonButton, IonLabel, IonItem, IonIcon, IonButtons, IonMenuButton, IonTabButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { HandleDataService } from 'src/app/services/data/handle-data.service';
 import { LocalStorageService } from 'src/app/shared/local-storage.service';
@@ -12,7 +12,7 @@ import { CommonService } from 'src/app/shared/common.service';
   templateUrl: './ride-detail-view.page.html',
   styleUrls: ['./ride-detail-view.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonIcon, IonItem, IonLabel, IonButton, IonRow, IonCol, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton]
+  imports: [IonTabButton, IonButtons, IonIcon, IonItem, IonLabel, IonButton, IonRow, IonCol, IonCard, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonMenuButton]
 })
 export class RideDetailViewPage implements OnInit {
 
@@ -27,6 +27,7 @@ export class RideDetailViewPage implements OnInit {
   currentUser: any;
   isEmailVerified: boolean = false;
   isPhoneVerified: boolean = false;
+  status: any = 'requested';
 
   constructor(private route: ActivatedRoute) { }
 
