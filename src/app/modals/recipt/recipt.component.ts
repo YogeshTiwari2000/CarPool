4da8e74a2e976eb82837b20b811040b59112b8b1
Aman Chaudhary <aman.chaudhary@paavu.com>
2024-08-26 11:40:30 +0530

    recipt

--- src/app/modals/recipt/recipt.component.ts
+++ src/app/modals/recipt/recipt.component.ts
@@ -1,22 +1,43 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonButtons, IonTitle, IonMenuButton, ModalController, IonBackButton, IonContent, IonCard, IonItem, IonCardHeader, IonIcon, IonLabel, IonCardContent, IonButton, IonItemDivider, IonAvatar } from "@ionic/angular/standalone";
+import {
+  IonHeader, IonToolbar, IonButtons, IonTitle, IonMenuButton, ModalController,
+  IonBackButton, IonContent, IonCard, IonItem, IonCardHeader, IonIcon, IonLabel,
+  IonCardContent, IonButton, IonItemDivider, IonAvatar
+} from "@ionic/angular/standalone";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipt',
   templateUrl: './recipt.component.html',
   styleUrls: ['./recipt.component.scss'],
   standalone: true,
-  imports: [IonAvatar, IonItemDivider, IonButton, IonCardContent, IonLabel, IonIcon, IonCardHeader, IonItem, IonCard, IonContent, IonBackButton, IonTitle, IonButtons, IonToolbar, IonHeader, IonMenuButton],
+  imports: [
+    IonAvatar, IonItemDivider, IonButton, IonCardContent, IonLabel, IonIcon,
+    IonCardHeader, IonItem, IonCard, IonContent, IonBackButton, IonTitle,
+    IonButtons, IonToolbar, IonHeader, IonMenuButton, CommonModule
+  ],
 })
 export class ReciptComponent implements OnInit {
-  @Input() userpaymentDetails: any
+  @Input() userpaymentDetails: any;
 
   public modalCtrl = inject(ModalController);
 
   constructor() { }
 
+  formatID(id: string): string {
+    let formattedID = id.replace("0.", "");
+    if (formattedID.length < 12) {
+      formattedID = formattedID.padEnd(12, '0');
+    } else {
+      formattedID = formattedID.slice(0, 12);
+    }
+    return formattedID;
+  }
+
   ngOnInit() {
-    console.log();
+    if (this.userpaymentDetails && this.userpaymentDetails.id) {
+      this.userpaymentDetails.id = this.formatID(this.userpaymentDetails.id);
+    }
     console.log("userpaymentDetails === ", this.userpaymentDetails);
   }
 
