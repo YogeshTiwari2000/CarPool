7955531f64167b75194f8a67465746111f3b2b08
Yogesh Tiwari <yogesh.tiwari@paavu.com>
2024-08-29 15:36:51 +0530

    vehicle

--- src/app/modals/vehicle/vehicle.component.ts
+++ src/app/modals/vehicle/vehicle.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { IonHeader, IonCol, IonToolbar, IonTitle, IonButton, IonIcon, IonContent, IonCard, IonRow, IonInput, ModalController, IonSelect, IonSelectOption } from "@ionic/angular/standalone";
+import { FormsModule } from '@angular/forms';  // Import FormsModule
 
 @Component({
   selector: 'app-vehicle',
   templateUrl: './vehicle.component.html',
   styleUrls: ['./vehicle.component.scss'],
   standalone: true,
-  imports: [IonInput, IonRow, IonCard, IonContent, IonIcon, IonButton, IonTitle, IonToolbar, IonCol, IonHeader, IonSelect, IonSelectOption],
+  imports: [IonInput, IonRow, IonCard, IonContent, IonIcon, IonButton, IonTitle, IonToolbar, IonCol, IonHeader, IonSelect, IonSelectOption, FormsModule],
 })
 export class VehicleComponent implements OnInit {
 
@@ -29,5 +30,14 @@ export class VehicleComponent implements OnInit {
     this.modalCtrl.dismiss();
   }
 
+  submit() {
+    if (this.vehicleDetails.vehicleType && this.vehicleDetails.vehicleNumber && this.vehicleDetails.vehicleName && this.vehicleDetails.vehicleColor) {
+      console.log(this.vehicleDetails);
 
+      this.modalCtrl.dismiss(this.vehicleDetails); // Passing data back to the parent
+    } else {
+      console.log("Error Occured");
+    }
+
+  }
 }
